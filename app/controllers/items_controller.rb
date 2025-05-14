af3fd06756c96a3d59e8312cdf32a6b6d22c0791
juniya-1001 <juniya1001@icloud.com>
2025-05-14 19:59:39 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,14 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
-    
+
+  end
+
+
+
+  private
+
+  def item_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 end
