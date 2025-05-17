44dad8b62689be532fb810f866f24e7bf66ce6ef
juniya-1001 <juniya1001@icloud.com>
2025-05-17 14:42:28 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,10 +8,13 @@ class Item < ApplicationRecord
   belongs_to :Shipping_charge
   belongs_to :shipping_time
 
-
+  validates :image, presence: { message: "can't be blank" }
+  validates :name, presence: { message: "can't be blank" }
+  validates :description, presence: { message: "can't be blank" }
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :state_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_area_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_time_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
 end
